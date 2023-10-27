66496080909f3217df99e1c43e8d7d3ad6fef640
blackmarllbor0 <3100194@gmail.com>
2023-10-28 00:52:41 +0600

    validate lang and setters for src and dest lang

--- API/translator.py
+++ API/translator.py
@@ -7,9 +7,29 @@ class Translator(ABC):
     def __init__(self, storage_service: StorageService):
         super().__init__()
         self.storage_service = storage_service
-        self.src_lang = storage_service.storage["lang"]["src"]
-        self.dest_lang = storage_service.storage["lang"]["dest"]
+
+        sl = storage_service.storage["lang"]["src"]
+        dl = storage_service.storage["lang"]["dest"]
+
+        if self._validate_lang(sl):
+            self.src_lang = sl.lower()
+        else:
+            self.src_lang = "auto"
+
+        if self._validate_lang(dl):
+            self.dest_lang = dl.lower()
+        else:
+            self.dest_lang = "auto"
 
     @classmethod
     def translate(cls, text: str) -> str:
         pass
+
+    def set_src_lang(self, lang: str) -> None:
+        self.src_lang = "auto" if self._validate_lang(lang) else lang
+
+    def set_dest_lang(self, lang: str) -> None:
+        self.dest_lang = "auto" if self._validate_lang(lang) else lang
+
+    def _validate_lang(self, lang: str) -> bool:
+        return lang.strip() and lang.isalpha()
