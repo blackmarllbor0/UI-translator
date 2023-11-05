2a52889e7b499926d6c3974d6c1feeda9b6103eb
blackmarllbor0 <3100194@gmail.com>
2023-11-05 16:43:05 +0600

    fix: delete comment

--- setup.py
+++ setup.py
@@ -1,27 +1,3 @@
-# from cx_Freeze import setup, Executable
-# import os, sys, subprocess
-
-# base = None
-# executables = [Executable("main.py", base=base, target_name="trans")]
-# packages = ["idna", "main", "UI", "API"]
-# options = {
-#     "build_exe": {"packages": packages, "build_exe": "build"},
-# }
-
-
-# setup(
-#     name="trans",
-#     version="1.0.0",
-#     author="blackmarllbor0",
-#     author_email="3100194@gmail.com",
-#     url="https://github.com/blackmarllbor0/UI-translator",
-#     long_description=open("README.md").read(),
-#     install_requires=open("requirements.txt").read().splitlines(),
-#     keywords=["translator"],
-#     options=options,
-#     executables=executables,
-# )
-
 from setuptools import setup, find_packages
 import os
 
